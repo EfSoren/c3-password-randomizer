cf5c5e23eb3983f854bd367d3bf40eeb145ae329
EfSoren <ef.sorensen@me.com>
2022-10-02 18:57:46 -0600

    finished randomizer and print to box

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
-var numberValues = [1, 2, 3, 4, 5, 6, 7, 8, 9,];
-var lowerValues = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',];
-var upperValues = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',];
-var symbolValues = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')',];
-var characterAmount = [];
-var charCodes = [];
-var passCode = [];
-var pass = []
 
-// Write password to the #password input
+var generateBtn = document.querySelector("#generate");
+var numberValues = "123456789";
+var lowerValues = "abcdefghijklmnopqrstuvwxyz";
+var upperValues = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var symbolValues = "!@#$%^&*()";
+var passLength = [];
+var pass = [];
+
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -18,40 +16,42 @@ function writePassword() {
 }
 
 
-// Add event listener to generate button
-//generateBtn.addEventListener("click", writePassword)
+
 
 generateBtn.addEventListener("click", function(){
-  console.log('working');
+  //resets values each time you request a password
+  var passValues = [];
   
-  var characterAmount = prompt("How many characters would you like in your password? (Between 8-128):", "8");
+  var passLength = prompt("How many characters would you like in your password? (Between 8-128):", "8");
   
-  if (characterAmount >= 8 && characterAmount <= 128 && characterAmount !== null && characterAmount !== ''){
+  if (passLength >= 8 && passLength <= 128 && passLength !== null && passLength !== ''){
   } else 
   { alert("Password must be between 8 and 128 characters") 
   return};
-  
+
+  //combines character types chosen into a single string
   var symbols = confirm("Would you like to include symbols?")
-  if(symbols === true){ charCodes = charCodes + (symbolValues)}
+  if(symbols === true){ passValues = passValues + (symbolValues)}
   
   var numbers = confirm("Would you like to include numbers?");
-  if(numbers === true){ charCodes = charCodes + (numberValues)}
+  if(numbers === true){ passValues = passValues + (numberValues)}
   
   var upcase = confirm("Would you like to include uppercase characters?");
-  if(upcase === true){ charCodes = charCodes + (upperValues)}
+  if(upcase === true){ passValues = passValues + (upperValues)}
   
   var lowcase = confirm("Would you like to include lowercase characters?"); 
-  if(lowcase === true){ charCodes = charCodes + (lowerValues)}
+  if(lowcase === true){ passValues = passValues + (lowerValues)}
   
-  const passwordCharacters = []
-  for (var i = 0; i < characterAmount; i++) {
-    const characterCode = charCodes[Math.floor(Math.random() * charCodes.length)];
-    passwordCharacters.push(String(characterCode))
+  //randomly selects a character from the available array equal to the number of characters you wanted
+  const availableChars = []
+  for (var i = 0; i < passLength; i++) {
+    const charCode = passValues[Math.floor(Math.random() * passValues.length)];
+    availableChars.push(charCode)
   }
-  var done = passwordCharacters.join('');
-  console.log(done);
-  console.log(charCodes.length);
-  console.log(passwordCharacters);
-  console.log(characterAmount);
+// joins values into a single string
+  var pass = availableChars.join('');
+  
+  password.placeholder = pass
+  return
 })
 
